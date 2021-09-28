647a897178dac0ae79916fc3cb586c209cc03e3e
matheusOliv23 <matheushenri1996@gmail.com>
2021-09-28 13:45:46 -0300

    integração da api com os cards

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,23 @@ import Header from '../components/Header/Header'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import Posts from '../components/Post/Post'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Home() {
   const [cards, setCards] = useState([])
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/posts')
+      .then(response => {
+        setPosts(response.data)
+      })
+      .catch(() => {
+        console.log('deu errado')
+      })
+  }, [])
 
   return (
     <div>
@@ -18,6 +31,9 @@ export default function Home() {
       <Link href="/form">Ir para formulario</Link>
       {cards.map(card => (
         <Posts conteudo={card} />
+      ))}
+      {posts.map(posts => (
+        <Posts conteudo={posts} />
       ))}
       <Card1 />
       <Footer />
